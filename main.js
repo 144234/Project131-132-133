bb824d8142c7381f4dfa1bbf0a3ec3fb8e64491a
144234 <78003838+144234@users.noreply.github.com>
2024-01-02 18:48:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location = "bedroom.html";
+}
+
+function bottle(){
+    window.location = "bottle.html";
+}
+
+function desk(){
+    window.location = "desk.html";
+}
+
+function Tv(){
+    window.location = "tv.html";
+}
